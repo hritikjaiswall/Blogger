3c672549405491dd6ad89026a0ab0870712bfea2
hritikjaiswall <fakehrj@gmail.com>
2025-02-13 17:15:46 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
